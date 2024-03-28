a333a16d357311a701d15e3274067f0f2eb0873e
Kazuki_Omata <mail.derakusu.zukinbaka@gmail.com>
2024-03-28 23:55:36 +0900

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # cube_to_clf
+
+
+intel macに対してのappが動作していない。
+terminalの方は動いている。
+
+
+
+2024/03/28時点では
+brewからopnecolorioをinstlallしている前提で動くようにしている。
+
+
+The only environment variable you must configure manually is OCIO, which points to the configuration file you wish to use. For prebuilt config files, see the Downloads section
+To do this, you would add a line to ~/.bashrc (or a per-project configuration script etc), for example:
+
+
+手動で設定しなければならない唯一の環境変数はOCIOで、これは使用したい設定ファイルを指す。ビルド済みの設定ファイルについては、ダウンロードのセクションを参照してください。
+https://opencolorio.readthedocs.io/en/latest/quick_start/installation.html
+
+
+OCIO 環境変数については自分でconfig.ocioがある場所を示さないといけない。
+```
+export OCIO="/path/to/my/config.ocio"
+```
--- cube_to_clf_python/test01.py
+++ cube_to_clf_python/test01.py
@@ -1,14 +1,13 @@
-
-
 import PyOpenColorIO as OCIO
 
-
 try:
-    config = OCIO.GetCurrentConfig()
-    processor = config.getProcessor(OCIO.Constants.ROLE_COMPOSITING_LOG,
-                                    OCIO.Constants.ROLE_SCENE_LINEAR)
+   config = OCIO.GetCurrentConfig()
+   processor = config.getProcessor(OCIO.ROLE_COMPOSITING_LOG,
+                                   OCIO.ROLE_SCENE_LINEAR)
+   cpu = processor.getDefaultCPUProcessor()
 
-    # Apply the color transform to the existing RGBA pixel data
-    img = processor.applyRGBA(img)
-except Exception, e:
-    print "OpenColorIO Error",e
+   # Apply the color transform to the existing RGBA pixel data
+   img = [1, 0, 0, 0]
+   img = cpu.applyRGBA(img)
+except Exception as e:
+   print("OpenColorIO Error: ", e)
